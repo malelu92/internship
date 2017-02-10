f4d00d23d26b461dc525d0c9d7fb20faa6665d78
Marina Leao Lucena <mleaoluc@ucn.inria.fr>
2017-02-10 12:28:38 +0100

    pegando nomes de tabelas da database hostview

--- simpleDBfetch.py
+++ simpleDBfetch.py
@@ -5,15 +5,15 @@ from sqlalchemy import MetaData
 from sqlalchemy.pool import NullPool
 
 def main():
-    DB = 'postgresql+psycopg2:///ucnstudy'
+    DB = 'postgresql+psycopg2:///hostview'
 
     engine = create_engine(DB, echo=False, poolclass=NullPool)
     metadata = MetaData()
     metadata.reflect(engine)
     for table in metadata.tables.values():
         print(table.name)
-        for column in table.c:
-            print (column.name)
+        #for column in table.c:
+        #    print (column.name)
 
 if __name__ == "__main__":
     main()
