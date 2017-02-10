29382365ae7123140b728af8b4659b8d0dab824e
Marina Leao Lucena <mleaoluc@ucn.inria.fr>
2017-02-10 15:42:42 +0100

    classe flow pegando a tabela e vendo counteudo

--- simpleDBfetch.py
+++ simpleDBfetch.py
@@ -3,14 +3,17 @@ import sys
 from sqlalchemy import create_engine
 from sqlalchemy import MetaData
 from sqlalchemy import Table
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import NullPool
 
 
 def main():
     DB = 'postgresql+psycopg2:///hostview'
-
     engine = create_engine(DB, echo=False, poolclass=NullPool)
+    Session = sessionmaker(bind=engine)
     metadata = MetaData()
+
+    ses = Session()
     metadata.reflect(engine)
     #for table in metadata.tables.values():
     #    if table.name == "flow":
@@ -20,8 +23,13 @@ def main():
     flow_table = Table('flow', metadata, autoload=True, autoload_with=engine)
     print (flow_table.name)
 
-    for column in flow_table.c:
-        print (column.name)
+   # flowid = select([flowtable.c.flowid])
+   # print (flowid[0], flowid[1])
+
+    #for column in flow_table.c:
+    #    print (column.name)
+
+    ses.close()
 
 if __name__ == "__main__":
     main()
