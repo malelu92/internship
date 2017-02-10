a08a961b527990f5b4424de824f18916940e4d11
Marina Leao Lucena <mleaoluc@ucn.inria.fr>
2017-02-10 14:13:22 +0100

    pega tabela especifica no bd

--- simpleDBfetch.py
+++ simpleDBfetch.py
@@ -2,7 +2,9 @@ import sys
 
 from sqlalchemy import create_engine
 from sqlalchemy import MetaData
+from sqlalchemy import Table
 from sqlalchemy.pool import NullPool
+
 
 def main():
     DB = 'postgresql+psycopg2:///hostview'
@@ -10,10 +12,16 @@ def main():
     engine = create_engine(DB, echo=False, poolclass=NullPool)
     metadata = MetaData()
     metadata.reflect(engine)
-    for table in metadata.tables.values():
-        print(table.name)
-        #for column in table.c:
-        #    print (column.name)
+    #for table in metadata.tables.values():
+    #    if table.name == "flow":
+    #        for column in table.c:
+    #            print column.name
+    
+    flow_table = Table('flow', metadata, autoload=True, autoload_with=engine)
+    print (flow_table.name)
+
+    for column in flow_table.c:
+        print (column.name)
 
 if __name__ == "__main__":
     main()
